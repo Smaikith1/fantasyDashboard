c699a0ba0a84c66d50aba4ea9016ba8cd266045e
Smaikith1 <kai_smith@edwards.com>
2024-09-12 17:11:18 -0700

    Initial Commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.set_page_config(layout='wide')
 
@@ -10,17 +11,33 @@ def init_state(key, value):
 def main(wrs, rbs, teams, weekly_scores, last_week_pos, point_key):
     week = weekly_scores.shape
     week = week[0]
-    st.title(f'Week {week} Goodell Gobblers Updates')
+    st.title(f'Week {week+1} Goodell Gobblers Updates')
 
-    col1, col2 = st.columns(2, gap='medium')
+    with st.expander('Last Week Scores'):
+        col1, col2 = st.columns(2, gap='medium')
 
-    with col1:
-        st.header('Last Weeks Scores', divider=True)
-        st.bar_chart(teams.sort_values(by=teams.columns[-1], ascending=False), horizontal=False)
+        with col1:
+            st.header('Last Weeks Scores', divider=True)
+            st.bar_chart(teams.sort_values(by=teams.columns[-1], ascending=False), horizontal=False)
 
-    with col2:
-        st.header('Last Weeks Positional Player Performance', divider=True)
-        st.bar_chart(last_week_pos, stack=False)
+        with col2:
+            st.header('Last Weeks Positional Player Performance', divider=True)
+            st.bar_chart(last_week_pos, stack=False)
+
+    with st.expander('Season Totals'):
+
+        col1, col2 = st.columns(2, gap='medium')
+
+        with col1:
+            st.header('Season Total Scores')
+            st.bar_chart(np.sum(teams, axis=1))
+
+        with col2:
+            df = pd.concat([np.sum(wrs, axis=1), np.sum(rbs, axis=1)], axis=1)
+            df.columns = ['wrs', 'rbs']
+
+            st.header('Season Total Positional Player Performance')
+            st.bar_chart(df, stack=False)
 
     st.header('Payout Points by Week', divider=True)
     st.line_chart(weekly_scores)
