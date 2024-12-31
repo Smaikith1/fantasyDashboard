bc59d24085c3d894de1e5112447969e5702ed357
Smaikith1 <kai_smith@edwards.com>
2024-12-31 11:23:35 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from espn_api.football import League
 import numpy as np
 import pandas as pd
 
-point_key = {'Weekly Leader': 1,
-             'Best Running Back Room EoY': 3,
-             'Best Reciever Room EoY': 3,
-             'League Winner': 15,
-             'Second Place': 5,
-             'Third Place': 3}
 
 def getTables(league, point_key):
     def accumulate(arr):
@@ -81,24 +75,61 @@ def getTables(league, point_key):
     weeklies_df = pd.DataFrame(weeklies, columns=[i.team_name for i in league.teams]).transpose()
     return wr_scores, rb_scores, team_scores, weeklies_df
 
+
+def make_scoreboard(point_key, weekly_scores, winners):
+    wrs, rbs, teams, _ = getTables(league, point_key)
+
+    results = {'Best Running Back Room EoY': rbs.index[rbs.iloc[:,0] == rbs.iloc[:,0].max()][0],
+               'Best Reciever Room EoY':     wrs.index[wrs.iloc[:,0] == wrs.iloc[:,0].max()][0],
+               'League Winner':              winners['First'],
+               'Second Place':               winners['Second'],
+               'Third Place':                winners['Third']}
+
+    scoreboard = dict(weekly_scores.iloc[len(weekly_scores)-1,:])
+
+    for key, item in results.items():
+        scoreboard[item] += point_key[key]
+
+    scoreboard = pd.Series(scoreboard).sort_values(ascending=False)
+
+    buy_in = 25
+    pot = buy_in * len(scoreboard)
+
+    payout = scoreboard*(pot/scoreboard.sum())
+    payout = payout.round(2)
+
+    return scoreboard, payout
+
+
+
 if __name__ == '__main__':
     league_id = 94554227
     year = 2024
 
     league = League(league_id=league_id, year=year)
 
+    point_key = {'Weekly Leader': 1,
+                 'Best Running Back Room EoY': 3,
+                 'Best Reciever Room EoY': 3,
+                 'League Winner': 15,
+                 'Second Place': 6,
+                 'Third Place': 3}
+
+    total_points = point_key['Weekly Leader']*(league.current_week - 1) + sum([i[1] for i in point_key.items() if i[0] not in ['Weekly Leader']])
+
     wrs, rbs, teams, weekly_scores = getTables(league, point_key)
     lsat_week_pos = pd.concat([wrs.iloc[:,-1], rbs.iloc[:,-1]], axis=1)
     lsat_week_pos.columns = ['wrs', 'rbs']
 
     weekly_scores = weekly_scores.T
+    weekly_scores.index = range(1,len(weekly_scores)+1)
     for c in weekly_scores.columns:
         weekly_scores[c] = [int(i) for i in weekly_scores[c]]
 
-    # for ix, _ in enumerate(weekly_scores.index):
-    #     row = weekly_scores.iloc[ix, :]
-    #     weekly_scores.loc[ix] = [int(i) for i in row]
+    winners ={'First': 'Mind Goblins', 'Second': 'Team Eggplant Parm', 'Third': 'Catalina Wine Mixon'}
+    scoreboard, payout = make_scoreboard(point_key, weekly_scores, winners)
+    print(payout)
 
     import app
 
-    app.main(wrs, rbs, teams, weekly_scores, lsat_week_pos, point_key)
+    # app.main(wrs, rbs, teams, weekly_scores, lsat_week_pos, point_key)
